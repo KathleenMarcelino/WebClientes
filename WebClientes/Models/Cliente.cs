1f5998ec6d021f0ead7409a92d0722f076d906e9
Kathleen <kat.marcelino12@gmail.com>
2023-01-22 23:49:43 -0300

    Adicionar arquivos de projeto.

---
+++ WebClientes/Models/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebClientes.Models
+{
+	[Table("clientes")]
+	public class Cliente
+	{
+		[Column("Id")]
+		[Display(Name = "ID")]
+		public int Id { get; set; }
+		[Column("Name")]
+		[Display(Name = "Nome")]
+		public string Name { get; set; }
+		[Column("Email")]
+		[Display(Name="E-mail")]
+		public string Email { get; set; }
+	}
+}
---
+++ WebClientes/Models/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace WebClientes.Models
+{
+	public class Contexto : DbContext
+	{
+		public Contexto(DbContextOptions<Contexto> options) : base(options)
+		{
+
+		}
+
+		public DbSet<Cliente> Cliente { get; set; }
+	}
+}
