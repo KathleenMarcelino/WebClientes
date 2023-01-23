09d1c2e8d986cc2fb0e75b1d932f5ab8c95da71e
Kathleen <kat.marcelino12@gmail.com>
2023-01-23 13:42:36 -0300

    sincronizando

---
+++ SQL/DML.sql
@@ -0,0 +1,20 @@
+﻿INSERT INTO clientes
+(Name, Email)
+VALUES
+('Kathleen', 'kat@gmail.com');
+
+INSERT INTO clientes
+(Name, Email)
+VALUES
+('Joao', 'joao@gmail.com');
+
+INSERT INTO clientes
+(Name, Email)
+VALUES
+('larissa', 'larissa@gmail.com');
+
+INSERT INTO clientes
+(Name, Email)
+VALUES
+('Pedro', 'pedro@gmail.com');
+
